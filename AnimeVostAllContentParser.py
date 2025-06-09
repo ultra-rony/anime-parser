45bbd76ae82cd8a8e91a71089eb26557ceb598c8
Roman Diakonov <vegappunk@gmail.com>
2025-06-09 11:29:33 +0900

    chore: update base url

--- AnimeVostAllContentParser.py
+++ AnimeVostAllContentParser.py
@@ -5,7 +5,7 @@ import re
 import colorama 
 from colorama import Fore, Style
 
-domain = "https://v5.vost.pw/"
+domain = "https://v7.vost.pw/"
 # Получить все года выходов аниме
 def getAllYears(url):
     result = requests.get(url)
